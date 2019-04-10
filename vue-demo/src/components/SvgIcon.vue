636647b19231f09b4185b03d3d1663c29973548c
shooterRao <rjw0319@163.com>
2019-04-10 20:06:43 +0800

    测试svgicon

---
+++ vue-demo/src/components/SvgIcon.vue
@@ -0,0 +1,52 @@
+<template>
+  <svg :style="svgColor" fill="currentColor" :class="svgClass" aria-hidden="true">
+    <use :xlink:href="iconName"></use>
+  </svg>
+</template>
+
+<script>
+export default {
+  name: "SvgIcon",
+  props: {
+    iconClass: {
+      type: String,
+      required: true
+    },
+    className: {
+      type: String
+    },
+    color: {
+      type: String
+    }
+  },
+  computed: {
+    iconName() {
+      return `#${this.iconClass}`;
+    },
+    svgClass() {
+      if (this.className) {
+        return `SvgIcon ${this.className}`;
+      }
+      return "SvgIcon";
+    },
+    svgColor() {
+      if (this.color) {
+        return {
+          color: this.color
+        };
+      }
+      return "";
+    }
+  }
+};
+</script>
+
+<style scoped>
+.SvgIcon {
+  width: 1em;
+  height: 1em;
+  vertical-align: -0.15rem;
+  fill: currentColor;
+  overflow: hidden;
+}
+</style>
--- vue-demo/src/views/Home.vue
+++ vue-demo/src/views/Home.vue
@@ -2,17 +2,25 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Hello Jenkins!!!!!!"/>
+    <SvgIcon iconClass="icon-peradd"/>
+    <SvgIcon iconClass="icon-star"/>
+    <SvgIcon iconClass="icon-loading"/>
+    <SvgIcon iconClass="icon-sousuo"/>
+    <SvgIcon iconClass="icon-refresh"/>
+    <SvgIcon iconClass="icon-item"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import SvgIcon from "@/components/SvgIcon.vue"
 import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    HelloWorld,
+    SvgIcon
   }
 }
 </script>
